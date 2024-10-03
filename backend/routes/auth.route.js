a2989c36dfb763969bc8e27c38e09d95ed66743f
李喆宸 <leeggroy@gmail.com>
2024-10-03 20:07:25 +0800

    controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,15 +1,10 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup",(req,res)=> {
-  res.send("Signup route")
-})
-router.get("/login",(req,res)=> {
-  res.send("Login route")
-})
-router.get("/logout",(req,res)=> {
-  res.send("Logout route")
-})
+router.get("/signup",signup)
+router.get("/login",login)
+router.get("/logout",logout)
 
 export default router;
