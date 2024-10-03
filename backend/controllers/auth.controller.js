c1f8903fb15e0b5fe1918fded260453cc74dcb7d
李喆宸 <leeggroy@gmail.com>
2024-10-03 22:49:02 +0800

    auth done

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -64,11 +64,57 @@ export async function signup(req, res) {
 	}
 }
 
+export async function login(req, res) {
+	try {
+		const { email, password } = req.body;
 
-export async function login(req,res){
-  res.send("Login route")
+		if (!email || !password) {
+			return res.status(400).json({ success: false, message: "All fields are required" });
+		}
+
+		const user = await User.findOne({ email: email });
+		if (!user) {
+			return res.status(404).json({ success: false, message: "Invalid credentials" });
+		}
+
+		const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+		if (!isPasswordCorrect) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(user._id, res);
+
+		res.status(200).json({
+			success: true,
+			user: {
+				...user._doc,
+				password: "",
+			},
+		});
+	} catch (error) {
+		console.log("Error in login controller", error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
 }
 
-export async function logout(req,res){
-  res.send("Logout route")
+
+export async function logout(req, res) {
+	try {
+		res.clearCookie("jwt-netflix");
+		res.status(200).json({ success: true, message: "Logged out successfully" });
+	} catch (error) {
+		console.log("Error in logout controller", error.message);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
 }
+
+// export async function authCheck(req, res) {
+// 	try {
+// 		console.log("req.user:", req.user);
+// 		res.status(200).json({ success: true, user: req.user });
+// 	} catch (error) {
+// 		console.log("Error in authCheck controller", error.message);
+// 		res.status(500).json({ success: false, message: "Internal server error" });
+// 	}
+// }
