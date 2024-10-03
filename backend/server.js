b81da20e5d4f80721e481d1975d3385e7ba6381a
李喆宸 <leeggroy@gmail.com>
2024-10-03 19:44:03 +0800

    route

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.get("/",(req,res)=> {
-  res.send("Server is ready now")
+app.get("/api/v1/signup",(req,res)=> {
+  res.send("Signup route")
+})
+app.get("/api/v1/login",(req,res)=> {
+  res.send("Login route")
+})
+app.get("/api/v1/logout",(req,res)=> {
+  res.send("Logout route")
 })
 
 app.listen(4000, () => {
