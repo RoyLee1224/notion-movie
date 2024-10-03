89f5c31cd3234741c2d0e58be2cc8ceebd17cbc8
李喆宸 <leeggroy@gmail.com>
2024-10-03 20:33:15 +0800

    db

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.route.js"
+import { ENV_VARS } from "./config/envVars.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = ENV_VARS.PORT
 
 app.use("/api/v1/auth",authRoutes)
 
-app.listen(4000, () => {
-	console.log("Server started at http://localhost:4000:");
+app.listen(PORT, () => {
+	console.log("Server started at http://localhost:"+PORT);
 });
