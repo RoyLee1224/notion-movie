170331aa08525d11f12577260672c394ed180b6f
李喆宸 <leeggroy@gmail.com>
2024-10-04 18:23:59 +0800

    search controller done

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
 import movieRoutes from "./routes/movie.route.js"
 import tvRoutes from "./routes/tv.route.js"
+import searchRoutes from "./routes/search.route.js"
 
 import { ENV_VARS } from "./config/envVars.js";
 import { connectDB } from "./config/db.js";
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use("/api/v1/auth",authRoutes)
 app.use("/api/v1/movie",protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute,tvRoutes)
+app.use("/api/v1/search", protectRoute,searchRoutes)
 // app.use("/api/v1/list",listRoutes)
 
 app.listen(PORT, () => {
