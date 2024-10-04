17a78db748a51dd4609be8b7d7490b20a57c5c99
李喆宸 <leeggroy@gmail.com>
2024-10-04 20:30:15 +0800

    notion service connect test

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -8,4 +8,6 @@ export const ENV_VARS = {
   JWT_SECRET:process.env.JWT_SECRET,
   NODE_ENV: process.env.NODE_ENV,
   TMDB_API_KEY: process.env.TMDB_API_KEY,
+  NOTION_API_KEY: process.env.NOTION_API_KEY,
+  NOTION_DATABASE_ID: process.env.NOTION_DATABASE_ID,
 }
