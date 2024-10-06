74783d0a90b92d93661db8ef949e0332901e6dbd
李喆宸 <leeggroy@gmail.com>
2024-10-06 18:53:01 +0800

    sign up function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from './pages/home/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Footer from './components/footer';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
       <Footer />
+      <Toaster />
     </>
   );
 }
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-// import { useAuthStore } from '../store/authUser';
+import { useAuthStore } from '../store/authUser';
 
 const SignUpPage = () => {
   const { searchParams } = new URL(document.location);
@@ -10,12 +10,11 @@ const SignUpPage = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  // const { signup, isSigningUp } = useAuthStore();
+  const { signup, isSigningUp } = useAuthStore();
 
   const handleSignUp = (e) => {
     e.preventDefault();
-    console.log(email, username, password);
-    // signup({ email, username, password });
+    signup({ email, username, password });
   };
 
   return (
@@ -88,9 +87,9 @@ const SignUpPage = () => {
               className="w-full py-2 bg-red-600 text-white font-semibold rounded-md
 							hover:bg-red-700
 						"
-              // disabled={isSigningUp}
+              disabled={isSigningUp}
             >
-              {/* {isSigningUp ? 'Loading...' : 'Sign Up'} */}
+              {isSigningUp ? 'Loading...' : 'Sign Up'}
             </button>
           </form>
           <div className="text-center text-gray-400">
