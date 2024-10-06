b32a79eede996d3367248494cc42ef3d99792cbf
李喆宸 <leeggroy@gmail.com>
2024-10-06 21:06:39 +0800

    trending content function done

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,28 +1,28 @@
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/NavBar';
 import { Info, Play } from 'lucide-react';
-// import useGetTrendingContent from '../../hooks/useGetTrendingContent';
-// import {
-//   MOVIE_CATEGORIES,
-//   ORIGINAL_IMG_BASE_URL,
-//   TV_CATEGORIES,
-// } from '../../utils/constants';
-// import { useContentStore } from '../../store/content';
+import useGetTrendingContent from '../../hooks/useGetTrendingContent';
+import {
+  MOVIE_CATEGORIES,
+  ORIGINAL_IMG_BASE_URL,
+  TV_CATEGORIES,
+} from '../../utils/constants';
+import { useContentStore } from '../../store/content';
 // import MovieSlider from '../../components/MovieSlider';
 import { useState } from 'react';
 
 const HomeScreen = () => {
-  // const { trendingContent } = useGetTrendingContent();
-  // const { contentType } = useContentStore();
+  const { trendingContent } = useGetTrendingContent();
+  const { contentType } = useContentStore();
   const [imgLoading, setImgLoading] = useState(true);
 
-  // if (!trendingContent)
-  //   return (
-  //     <div className="h-screen text-white relative">
-  //       <Navbar />
-  //       <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer" />
-  //     </div>
-  //   );
+  if (!trendingContent)
+    return (
+      <div className="h-screen text-white relative">
+        <Navbar />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/70 flex items-center justify-center -z-10 shimmer" />
+      </div>
+    );
 
   return (
     <>
@@ -35,8 +35,7 @@ const HomeScreen = () => {
         )}
 
         <img
-          // src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
-          src="extraction.jpg"
+          src={ORIGINAL_IMG_BASE_URL + trendingContent?.backdrop_path}
           alt="Hero img"
           className="absolute top-0 left-0 w-full h-full object-cover -z-49 "
           onLoad={() => {
@@ -57,26 +56,24 @@ const HomeScreen = () => {
 
           <div className="max-w-2xl">
             <h1 className="mt-4 text-6xl font-extrabold text-balance">
-              {/* {trendingContent?.title || trendingContent?.name} */}
-              Extraction
+              {trendingContent?.title || trendingContent?.name}
             </h1>
             <p className="mt-2 text-lg">
-              {/* {trendingContent?.release_date?.split('-')[0] ||
+              {trendingContent?.release_date?.split('-')[0] ||
                 trendingContent?.first_air_date.split('-')[0]}{' '}
-              | {trendingContent?.adult ? '18+' : 'PG-13'} */}
+              | {trendingContent?.adult ? '18+' : 'PG-13'}
             </p>
 
             <p className="mt-4 text-lg">
-              {/* {trendingContent?.overview.length > 200
+              {trendingContent?.overview.length > 200
                 ? trendingContent?.overview.slice(0, 200) + '...'
-                : trendingContent?.overview} */}
-              content
+                : trendingContent?.overview}
             </p>
           </div>
 
           <div className="flex mt-8">
             <Link
-              // to={`/watch/${trendingContent?.id}`}
+              to={`/watch/${trendingContent?.id}`}
               className="bg-white hover:bg-white/80 text-black font-bold py-2 px-4 rounded mr-4 flex
 							 items-center"
             >
@@ -85,7 +82,7 @@ const HomeScreen = () => {
             </Link>
 
             <Link
-              // to={`/watch/${trendingContent?.id}`}
+              to={`/watch/${trendingContent?.id}`}
               className="bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded flex items-center"
             >
               <Info className="size-6 mr-2" />
